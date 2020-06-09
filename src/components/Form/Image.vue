869cc18dfef6c1e256493480cfb79f8ec077403a
lapinskap <cherry3475@gmail.com>
2020-06-09 16:29:17 +0200

    Add t-shirt preview

---
+++ src/components/Form/Image.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <img class="img-front" v-if="placement === 'front' || 'both sides'" :src="selectedImg" />
+    <img class="img-back" v-if="placement === 'back' || 'both sides'" :src="selectedImg" />
+    <img class="t-shirt" src="../../utils/image.jpg" />
+</div>
+</template>
+<script>
+export default {
+data() {
+    return {
+      placement: null,
+      selectedImg: '',
+    }
+  },
+}
+</script>
+<style scoped>
+.t-shirt {
+  max-width: 500px;
+}
+.img-front {
+  position: absolute;
+  z-index: 100;
+}
+.img-back {
+  position: absolute;
+  z-index: 100;
+}
+</style>
